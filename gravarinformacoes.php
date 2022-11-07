4ca55d708c231167c953e64302bff87419b19e04
Enzo <enzoc432@gmail.com>
2022-11-07 14:05:58 -0300

    main

--- gravarinformacoes.php
+++ gravarinformacoes.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once("conexao.php");
+include_once("conexao1.php");
 ?>
 <?php
     $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
--- listar.php
+++ listar.php
@@ -5,9 +5,25 @@
 <body>
     <h1>Listar Alunos</h1>
     <?php
+    $busca="";
         $mysql = new mysqli("localhost",
         "root","","cadastro");
-        if($resultado_sel = $mysqli-> query("SELECT id, nome, email FROM alunos"))
+        if($resultado_sel = $mysqli-> query("SELECT id, 
+        nome, email FROM alunos WHERE nome like'.$busca.%'")){
+            echo'<table border="1">';
+            echo '<tr>';
+            echo'<td>Nome</td>';
+            echo'<td>E-mail</td>';
+            echo'</tr>';
+
+            while($row = mysqli_fetch_assoc($resultado_sel)){
+                echo'<tr>';
+                echo'<td>'.$row['nome'].'</td>';
+                echo'<td>'.$row['email'].'</td>';
+                echo'</tr>';
+            }
+            echo'</table>';
+        }
     ?>
 </body>
 </html>
