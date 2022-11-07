9079d269a360b9e81bd6edb2bd66b1a17ab6a358
Enzo Costa <105054841+enzocst@users.noreply.github.com>
2022-11-07 13:23:55 -0300

    Create listar.php

---
+++ listar.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Listar Cadastros</title>
+    </head>
+<body>
+    <h1>Listar Alunos</h1>
+    <?php
+        $mysql = new mysqli("localhost",
+        "root","","cadastro");
+        if($resultado_sel = $mysqli )
+    ?>
+</body>
+</html>
