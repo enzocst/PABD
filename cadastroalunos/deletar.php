456ef42f24e3cb2f2d14c8840170170d7cb4bdf5
Enzo <enzoc432@gmail.com>
2022-11-09 13:50:31 -0300

    main

---
+++ cadastroalunos/deletar.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    include_once("conexao.php");
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    if(!empty($id)){
+        $excluir = "DELETE FROM alunos WHERE id='$id'";
+        $resultado = mysqli_query($conn, $excluir)
+    }
+?>
--- cadastroalunos/listar.php
+++ cadastroalunos/listar.php
@@ -21,6 +21,9 @@
                 echo'<td>'.$row['id'].'</td>';
                 echo'<td>'.$row['nome'].'</td>';
                 echo'<td>'.$row['email'].'</td>';
+                echo "<td> <a href='deletar.php?tipo=2&id=".$row['id']."'>
+                <img src='img/deleteimg.png' width='20px' height='20' title='Excluir'/>
+                </td>";
                 echo'</tr>';
             }
             echo'</table>';
