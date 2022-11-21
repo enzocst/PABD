0207238bb2d23a0387bb7e599f8a95468b230985
Enzo <enzoc432@gmail.com>
2022-11-21 16:51:31 -0300

    main

--- cadastro/listar.php
+++ cadastro/listar.php
@@ -22,7 +22,7 @@
                 echo'<td>'.$row['nome'].'</td>';
                 echo'<td>'.$row['email'].'</td>';
                 echo "<td> <a href='deletar.php?tipo=2&id=".$row['id']."'>
-                <img src='imagens/borracha.png' width='20' height='20' title='Excluir'/>
+                <img src='img/deleteimg.png' width='20' height='20' title='Excluir'/>
                 </td>";
                 echo'</tr>';
             }
