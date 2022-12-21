b43068b3d31f224011d4d382e2239db1c88a7a70
Enzo Costa <105054841+enzocst@users.noreply.github.com>
2022-12-21 14:28:57 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Buscando dados</title>
+        <script type="text/javascript" src="javascriptpersonalizado.js"></script>
+    </head>
+<body>
+<h1>Pesquisa de cursos</h1>
+<form method="POST" id="form-pesquisa" action="">
+    <label>Pesquisar</label>
+    <input type="text" name="pesquisa" id="pesquisa" placeholder="O que buscas?">
+    <button type="sumbmit" name="enviar" value="Pesquisar">Pesquisar</button>
+</form>
+    <ul class = "resultado">
+</body>
+</html>
